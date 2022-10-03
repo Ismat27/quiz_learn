853ed65ad9d8af1e96baf9413d7cef1c865cebb9
unknown <adegbiteismail27@gmail.com>
2022-10-03 07:27:52 -0700

    connect to live db

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flaskr import create_app
 from config import DevelopmentConfig, TestingConfig, ProductionConfig
 
-app = create_app()
+app = create_app(ProductionConfig)
 
 if __name__ == '__main__':
    app.run()
