a9a1d27e18a584b4403e5ce64011dd413e883e52
unknown <adegbiteismail27@gmail.com>
2022-10-05 09:25:12 -0700

    update to development mode

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flaskr import create_app
 from config import DevelopmentConfig, TestingConfig, ProductionConfig
 
-app = create_app(ProductionConfig)
+app = create_app()
 
 if __name__ == '__main__':
    app.run()
